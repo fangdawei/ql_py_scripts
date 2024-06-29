f4915487bef58811b756ee952384bad2eb9e8621
fangdawei <fangdawei.www@gmail.com>
2024-06-30 00:10:02 +0800

    mt

--- mt_free_auto.py
+++ mt_free_auto.py
@@ -178,7 +178,7 @@ class MTFreeAutoTask:
         return (
             f"种子名: {free_info['name']}\n"
             + f"种子描述: {free_info['small_descr']}\n"
-            + f"文件大小: {free_info['size']/BYTES_GB}\n"
+            + f"文件大小: {int(free_info['size']/BYTES_GB)} GB\n"
             + f"做种数: {free_info['seeders']}\n"
             + f"下载数: {free_info['leechers']}\n"
             + f"FREE到期时间: {free_end_time}"
